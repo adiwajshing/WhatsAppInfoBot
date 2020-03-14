2d654f781de07e1d6929920465df4ba2309b5036
Adhiraj <adhirajsingh1001@gmail.com>
2020-03-14 20:16:30 +0530

    moved to Baileys

--- Responder.js
+++ Responder.js
@@ -11,16 +11,17 @@ module.exports = class Responder {
 			languageProcessor.metadata.maxRequestsPerSecond = 1.0
 			languageProcessor.metadata.responseTimeSeconds = [0.5,3.0]
 		}
+
 		this.authFile = authFile
 		this.admins = {}
 		this.log = { }
 		this.client = new WhatsAppWeb()
-		this.client.onConnected = () => {
+		this.client.handlers.onConnected = () => {
 			const authInfo = this.client.base64EncodedAuthInfo()
     		fs.writeFileSync(this.authFile, JSON.stringify(authInfo, null, "\t"))
 		}
-		this.client.onUnreadMessage = (m) => this.onMessage(m)
-		this.client.onError = (err) => this.log("error: " + err)
+		this.client.handlers.onUnreadMessage = (m) => this.onMessage(m)
+		this.client.handlers.onError = (err) => console.log("error: " + err)
 
 		for (var i in languageProcessor.metadata.admins) {
 			this.admins[ languageProcessor.metadata.admins[i] ] = true
@@ -33,9 +34,9 @@ module.exports = class Responder {
 		try {
 			const file = fs.readFileSync(this.authFile) // load a closed session back if it exists
 			const authInfo = JSON.parse(file)
-			client.login( authInfo )
+			this.client.login( authInfo )
 		} catch {
-			client.connect()
+			this.client.connect()
 		}
 	}
 	clearLog () {
@@ -86,22 +87,24 @@ module.exports = class Responder {
 		
 		response.then (str => {
 			console.log(number + " sent message '" + messageText + "', responding with " + str)
-			this.sendMessageAfterDelay(sender, str.charAt(0).toUpperCase() + str.slice(1))
+			str = str.charAt(0).toUpperCase() + str.slice(1)
+			this.sendMessage(sender, str, message.key.id)
 		}).catch (err => {
 			console.log(number + " sent message '" + messageText + "', got error " + err)
-			this.sendMessageAfterDelay(sender, err)
+			this.sendMessage(sender, err, message.key.id)
 		})
 	}
 	parseNumber (contact) {
 		return contact.split("@")[0]
 	}
-	sendMessageAfterDelay(toContact, message) {
-		const responseTimeRange = this.processor.metadata.responseTimeSeconds
-		const delayS = (responseTimeRange[1]-responseTimeRange[0]) * Math.random() + responseTimeRange[0]
-		setTimeout(() => this.sendMessage(toContact, message), delayS*1000)
-	}
-	sendMessage(toContact, message) {
-		this.client.sendTextMessage(toContact, message)
-		this.client.sendText(toContact, message)
+	sendMessage(toContact, message, messageID) {
+		let delay = this.processor.metadata.responseTimeSeconds[0]
+		setTimeout(() => this.client.updatePresence(toContact, WhatsAppWeb.Presence.available), delay*1000)
+		delay += 0.5
+		setTimeout(() => this.client.sendReadReceipt(toContact, messageID), delay*1000)
+		delay += 0.5
+		setTimeout(() => this.client.updatePresence(toContact, WhatsAppWeb.Presence.composing), delay*1000)
+		delay += 2.0
+		setTimeout(() => this.client.sendTextMessage(toContact, message), delay*1000)
 	}
 }
