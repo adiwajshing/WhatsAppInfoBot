5a9ff721a0fad91b298fc8e4380c7fe0859b0dfb
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-02-24 19:33:17 +0530

    Update SendMammyResponder.ts

--- src/SendMammyResponder.ts
+++ src/SendMammyResponder.ts
@@ -1,4 +1,5 @@
 import type { WAMessage } from "@adiwajshing/baileys"
+import type { AuthenticationController } from "@chatdaddy/authentication-utils"
 import { LanguageProcessor, WAResponderParameters } from "./types"
 import { onWAMessage } from "./WAResponder"
 import got from "got"
@@ -14,7 +15,7 @@ export const createSendMammyResponder = (processor: LanguageProcessor, metadata:
 	if(!makeAuthenticationController) {
 		throw new Error('Could not find @chatdaddy/authentication-utils')
 	}
-	const authController = makeAuthenticationController(
+	const authController: AuthenticationController = makeAuthenticationController(
 		metadata.refreshToken,
 		'https://api-auth.chatdaddy.tech'
 	)
@@ -22,7 +23,7 @@ export const createSendMammyResponder = (processor: LanguageProcessor, metadata:
 		const authToken = event.headers['Authorization']?.replace('Bearer ', '')
 		const user = await authController.authenticate(authToken)
 
-		const sendMessage = async(jid: string, text: string, quoted: WAMessage) => {
+		const sendMessage = async(jid: string, text: string, quoted?: WAMessage) => {
 			const token = await authController.getToken(user.teamId)
 			await got.post(
 				`https://api.sendmammy.com/messages/${jid}`,
@@ -30,7 +31,7 @@ export const createSendMammyResponder = (processor: LanguageProcessor, metadata:
 					body: JSON.stringify({
 						text,
 						scheduleAt: Math.floor(Date.now()/1000), // send message now
-						quotedID: quoted.key.id, // quote the message
+						quotedID: quoted?.key.id, // quote the message
 						sendTyping: true, // send with typing indicator
 						randomizeMessage: false
 					}),
@@ -44,21 +45,22 @@ export const createSendMammyResponder = (processor: LanguageProcessor, metadata:
 		
 		console.log('received web-hook for ' + user.teamId)
 
-		const { body } = event
-		switch(body.event) {
-			case 'chat-update':
-			case 'messages-post-sleep':
-				await Promise.all(
-					body.data.messages.map((message: WAMessage) => (
-						onWAMessage(message, { metadata, processor, sendMessage })
-					))
-				)
-				return {
-					statusCode: 200,
-					body: JSON.stringify({ success: true })
-				}
-			default:
-				return { statusCode: 204 }
+		const body = JSON.parse(event.body)
+
+		console.log('event is ', body.event)
+
+		if((body.event === 'chat-update' && body.data.hasNewMessage) ||
+			body.event === 'messages-post-sleep') {
+			await Promise.all(
+				body.data.messages.map((message: WAMessage) => (
+					onWAMessage(message, { metadata, processor, sendMessage })
+				))
+			)
+			return {
+				statusCode: 200,
+				body: JSON.stringify({ success: true })
+			}
 		}
+		return { statusCode: 204 }
 	}
 }
