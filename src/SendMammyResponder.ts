38b41b43c5d268a940bb3e34070fe7d2d96700ed
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-03-21 17:41:35 +0530

    More config options for SendMammyResponder

--- src/SendMammyResponder.ts
+++ src/SendMammyResponder.ts
@@ -7,66 +7,89 @@ import { URL } from "url"
 
 // serverless function for interacting with SendMammy APIs
 
-export type SendMammyResponderParameters = WAResponderParameters & {
+export const SENDMAMMY_URL = new URL(process.env.SENDMAMMY_URL || 'https://api.sendmammy.com')
+
+export type SendMammyResponderParameters = WAResponderParameters & ({
 	refreshToken: string
+} | {
+	authenticationController: AuthenticationController
+})
+export type SendMessageOptions = {
+	jid: string 
+	answer: Answer
+	quotedId?: string
+	token: string
+}
+
+export const sendWAMessage = async({ jid, answer, quotedId, token }: SendMessageOptions) => {
+	const timestamp = Math.floor(Date.now()/1000)
+	
+	let path = `messages/${jid}`
+	let body: any = {}
+	if(typeof answer === 'object' && 'template' in answer) {
+		path = `${path}/${answer.template}`
+		body = { parameters: answer.parameters }
+	} else if(typeof answer === 'string') {
+		body = { text: answer }
+	} else {
+		body = answer
+	}
+
+	const result = await got.post(
+		new URL(path, SENDMAMMY_URL),
+		{
+			body: JSON.stringify({
+				...body,
+				scheduleAt: timestamp, // send message now
+				quotedID: quotedId, // quote the message
+				withTyping: true, // send with typing indicator
+				randomizeMessage: false,
+				tag: Date.now().toString() + Math.floor(Math.random()*100).toString() // ensures the message is only sent out once 
+			}),
+			headers: {
+				'authorization': `Bearer ${token}`,
+				'content-type': 'application/json'
+			},
+			retry: {
+				limit: 10,
+				statusCodes: [504, 503, 502, 408],
+				errorCodes: [ 'ENOTFOUND', 'ETIMEDOUT' ],
+				calculateDelay: () => 250
+			},
+			throwHttpErrors: false
+		}
+	)
+	if(![200].includes(result.statusCode)) {
+		throw new Error(`error in pushing message: (${result.statusCode}) ${result.body}`)
+	}
 }
 
 const { makeAuthenticationController } = require('@chatdaddy/authentication-utils') || {}
-export const createSendMammyResponder = (processor: LanguageProcessor, metadata: SendMammyResponderParameters) => {
+export const createSendMammyResponder = (
+	processor: LanguageProcessor, 
+	metadata: SendMammyResponderParameters
+) => {
 	if(!makeAuthenticationController) {
 		throw new Error('Could not find @chatdaddy/authentication-utils')
 	}
-	const authController: AuthenticationController = makeAuthenticationController(
-		metadata.refreshToken,
-		process.env.AUTH_SERVICE_URL || 'https://api-auth.chatdaddy.tech'
-	)
-	const sendMammyUrl = new URL(process.env.SENDMAMMY_URL || 'https://api.sendmammy.com')
+	let authController: AuthenticationController
+	if('authenticationController' in metadata) {
+		authController = metadata.authenticationController
+	} else {
+		authController = makeAuthenticationController(
+			metadata.refreshToken,
+			process.env.AUTH_SERVICE_URL || 'https://api-auth.chatdaddy.tech'
+		)
+	}
+
 	return async (event: any) => {
 		const authToken = event.headers['Authorization']?.replace('Bearer ', '')
 		const user = await authController.authenticate(authToken)
 
 		const sendMessage = async(jid: string, answer: Answer, quoted?: WAMessage) => {
 			const token = await authController.getToken(user.teamId)
-			const timestamp = Math.floor(Date.now()/1000)
-			
-			let path = `messages/${jid}`
-			let body: any = {}
-			if(typeof answer === 'object' && 'template' in answer) {
-				path = `${path}/${answer.template}`
-				body = { parameters: answer.parameters }
-			} else if(typeof answer === 'string') {
-				body = { text: answer }
-			} else {
-				body = answer
-			}
-
-			const result = await got.post(
-				new URL(path, sendMammyUrl),
-				{
-					body: JSON.stringify({
-						...body,
-						scheduleAt: timestamp, // send message now
-						quotedID: quoted?.key.id, // quote the message
-						withTyping: true, // send with typing indicator
-						randomizeMessage: false,
-						tag: Date.now().toString() + Math.floor(Math.random()*100).toString() // ensures the message is only sent out once 
-					}),
-					headers: {
-						'authorization': `Bearer ${token}`,
-						'content-type': 'application/json'
-					},
-					retry: {
-						limit: 10,
-						statusCodes: [504, 503, 502, 408],
-						errorCodes: [ 'ENOTFOUND', 'ETIMEDOUT' ],
-						calculateDelay: () => 250
-					},
-					throwHttpErrors: false
-				}
-			)
-			if(![200].includes(result.statusCode)) {
-				throw new Error(`error in pushing message: (${result.statusCode}) ${result.body}`)
-			}
+			const result = await sendWAMessage({ jid, answer, quotedId: quoted.key.id, token })
+			return result
 		}
 		
 		console.log('received web-hook for ' + user.teamId)
@@ -90,7 +113,7 @@ export const createSendMammyResponder = (processor: LanguageProcessor, metadata:
 			const token = await authController.getToken(user.teamId)
 			// get all unread chats
 			const result = await got.get(
-				new URL(`chats?unread=true`, sendMammyUrl),
+				new URL(`chats?unread=true`, SENDMAMMY_URL),
 				{
 					headers: {
 						'authorization': `Bearer ${token}`,
